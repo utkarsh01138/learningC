82b006c3c9fa835ade9a5e0aaef372f6f971c539
utkarsh01138 <utkarshpatnaik0@gmail.com>
2026-09-11 16:41:55 +0530

    Starting to learn from w3cshools

--- 01_Basics/syntax.C
+++ 01_Basics/syntax.C
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> //Tells C to include the standard input/output library
 
-int main()
+int main() //
 {
-    
+    printf("Hello World!\n"); // printf() is a function that prints the text in the brackets to the console. 
+    return 0; // return 0; tells the operating system that the program has ended successfully.
 }
