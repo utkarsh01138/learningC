3dc9c955cd92d8e9e958d49faa9ff9c93cc72c59
utkarsh01138 <utkarshpatnaik0@gmail.com>
2026-09-19 01:06:06 +0530

    Question 01 of Lab 05 practice done.

--- Practice/Lab05/q1.C
+++ Practice/Lab05/q1.C
@@ -5,6 +5,12 @@ int main()
     // Variable declaration
     int numberOfPatients;
     int patientAges[numberOfPatients];
+    float ageAverage;
+    int maxAge, minAge;
+
+    // Taking number of patients input
+    printf("Enter the number of patients: ");
+    scanf("%d", &numberOfPatients);
 
     // Taking patient's age inputs
     for (int i = 0; i < numberOfPatients; i++)
@@ -13,7 +19,23 @@ int main()
         scanf("%d", &patientAges[i]);
     }
 
-    
+    // Computing average, min, and max ages
+    int sum = 0; 
+    maxAge = patientAges[0];
+    minAge = patientAges[0];
+    for (int i = 0; i < numberOfPatients; i++)
+    {
+        sum += patientAges[i];
+        if (patientAges[i] > maxAge)
+            maxAge = patientAges[i];
+        if (patientAges[i] < minAge)
+            minAge = patientAges[i];
+    }
+    ageAverage = (float)sum / numberOfPatients;
+
+    printf("Average age: %.2f \n", ageAverage);
+    printf("Maximum age: %d \n", maxAge);
+    printf("Minimum age: %d \n", minAge);
 
     return 0;
 }
