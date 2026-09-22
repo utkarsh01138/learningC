0b41989c1782b7622531348644aab8751ab2a8b5
utkarsh01138 <utkarshpatnaik0@gmail.com>
2026-09-22 18:56:53 +0530

    Finished Prac Lab 05 Q2.

--- Practice/Lab05/q2.C
+++ Practice/Lab05/q2.C
@@ -3,7 +3,27 @@
 int main()
 {
     // Variable declaration
-    int numOfNumbers;
-    int loopIteration;
-    
-}
+    int number;
+    int numPositive, numNegative;
+    numPositive = 0;
+    numNegative = 0;
+
+    // Taking the user input
+    do 
+    {
+        printf("Enter an integer: ");
+        scanf("%d", &number);
+
+        // Counting the number of positive and negative numbers
+        if (number > 0)
+            numPositive++;
+        else if (number < 0)
+            numNegative++;
+        
+        printf("Number of +ve numbers: %d\n", numPositive);
+        printf("Number of -ve numbers: %d\n", numNegative);
+    } 
+    while (number != 0); // Checking the exit condition
+
+    return 0;
+}   
