acba64634f0f5ff41448786344040a8924868f50
utkarsh01138 <utkarshpatnaik0@gmail.com>
2026-09-24 16:47:29 +0530

    Finished q3.

--- Practice/Lab05/q3.C
+++ Practice/Lab05/q3.C
@@ -4,7 +4,20 @@ int main()
 {
     // Variable declaration
     int number; 
-    int numOfDigits;
+    int numOfDigits = 0;
 
     // Taking user input
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Counting number of digits
+    while (number != 0)
+    {
+        number /= 10;
+        numOfDigits++;
+    }
+
+    // Output
+    printf("Number of digits: %d\n", numOfDigits);
+    
 }
