2b5642c3e6b6bbcb235d88ea0a64a1a8d9d96304
utkarsh01138 <utkarshpatnaik0@gmail.com>
2026-09-25 14:05:45 +0530

    Small changes.

--- CSD_20260925.C
+++ CSD_20260925.C
@@ -1,3 +1,7 @@
 #include <stdio.h> //Tells C to include the standard input/output library
 
-int f(int x)
+int main() //
+{
+    printf("Hello World!\n"); // printf() is a function that prints the text in the brackets to the console. 
+    return 0; // return 0; tells the operating system that the program has ended successfully.
+}
